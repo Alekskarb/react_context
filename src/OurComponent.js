e47793d78ad3656982f15b3c72cf79134ddaebf4
Alex Korbutovski <akorbutovski@elinext.com>
2023-09-20 11:07:01 +0300

    first commit

--- src/OurComponent.js
+++ src/OurComponent.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function OurComponent() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      OurComponent
     </div>
   );
 }
 
-export default App;
+export default OurComponent;
